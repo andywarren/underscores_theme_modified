5a1adeaf6d345ad148b2a829462a47a2b21c6de4
Andy Warren <andyw1981@gmail.com>
2014-12-23 10:15:47 -0600

    Update functions.php
    Changed nav menu description.

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ function womg_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'womg' ),
+		'primary' => __( 'Primary Menu', 'The Main Navigation Menu' ),
 	) );
 
 	/*
