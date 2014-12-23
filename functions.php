e629d6bb1c176c13a038fa2136c5a8655859cf25
Andy Warren <andy@andy-warren.net>
2014-12-23 13:55:50 -0600

    added enqueue jQuery document ready script

--- functions.php
+++ functions.php
@@ -103,6 +103,8 @@ function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	
+	wp_enqueue_script( 'documentReady-js', get_template_directory_uri() . '/js/documentReady.js', array('jquery'), '1.0', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
